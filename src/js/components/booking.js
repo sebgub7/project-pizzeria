8f890392a7aefc639d007ff8247dad74b0c295b3
Sebastian Gubiec <seb.gub@wp.pl>
2021-08-16 14:31:50 +0200

    end_of_11.1

--- src/js/components/booking.js
+++ src/js/components/booking.js
@@ -3,6 +3,8 @@ import {
   templates
 } from '../settings.js';
 import AmountWidget from './amountWidgets.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element) {
@@ -19,6 +21,8 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
   }
   initWidgets() {
     const thisBooking = this;
@@ -26,6 +30,8 @@ class Booking {
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.dom.peopleAmount.addEventListener('updated', function() {});
     thisBooking.dom.hoursAmount.addEventListener('updated', function() {});
+    thisBooking.dom.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.dom.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
 export default Booking;
